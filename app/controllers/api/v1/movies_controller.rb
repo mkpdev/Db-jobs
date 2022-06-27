1e5e49373b90090a29b60d6fa3a5fbd0cf211664
Mukesh patel <mukeshkup92@gmail.com>
2022-06-27 19:08:32 +0530

    Remove useless method

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -5,10 +5,6 @@ class Api::V1::MoviesController < ApplicationController
     render json: @movies, each_serializer: MovieSerializer
   end
 
-  def create
-    CreateQueueJob.set(priority: 1).perform_later(job_id)
-  end
-
   private
   def movie_params
     params.require(:movie).permit(:title)
